da696b89e307f24ed7b3659d1ce8feac76ed2b2e
Maureen Grandidier <maureen.grandidier@gmail.com>
2023-03-17 08:58:25 +0100

    Tp4 exo1 - insertHeapNode
    Permits to insere a new node in the heap

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -4,6 +4,7 @@
 #include <QApplication>
 #include <time.h>
 #include <stdio.h>
+#include <algorithm> 
 
 MainWindow* w = nullptr;
 using std::size_t;
@@ -23,6 +24,12 @@ void Heap::insertHeapNode(int heapSize, int value)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i = heapSize;
+	this->get(i) = value;
+	while(i > 0 && this->get(i) > this->get((i - 1) / 2))
+	{
+		std::swap(this->get(i), this->get((i - 1) / 2));
+		i = (i - 1) / 2;
+	}
 }
 
 void Heap::heapify(int heapSize, int nodeIndex)
