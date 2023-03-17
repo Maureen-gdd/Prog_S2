50fcd0c4dcc66801a7fa7cba9a707ded36cb6e7b
Maureen Grandidier <maureen.grandidier@gmail.com>
2023-03-17 09:10:57 +0100

    en cours

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -32,9 +32,10 @@ void Heap::insertHeapNode(int heapSize, int value)
 	}
 }
 
+// Si le noeud à l’indice nodeIndex n’est pas supérieur à ses enfants, reconstruit le tas à partir de cette index.
 void Heap::heapify(int heapSize, int nodeIndex)
 {
-	// use (*this)[i] or this->get(i) to get a value at index i
+	
 	int i_max = nodeIndex;
 }
 
